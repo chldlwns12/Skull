2f24d41bf4d9236c08bf887506f78a0369114b35
chldlwns12 <chldlwns12@gmail.com>
2020-04-22 17:33:50 +0900

    -2020.04.22 버전 0.1 로딩씬 메뉴씬 완성 ,맵툴 코드만 옮겨두었고 사진과 타일크기,타일갯수 변경해서 맵툴 완성해야한다.

---
+++ v0.1/inGameScene.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "gameNode.h"
+
+class inGameScene : public gameNode
+{
+private:
+
+public:
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+	inGameScene() {}
+	~inGameScene() {}
+};
+
