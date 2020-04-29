2f6a94ca8b06c82c685958ea5c85958f89eff3c2
chldlwns12 <chldlwns12@gmail.com>
2020-04-29 17:48:01 +0900

    2020.04.29 버전 0.3 대시수정, 카메라 수정

---
+++ v0.3/player.h
@@ -0,0 +1,105 @@
+#pragma once
+#include "gameNode.h"
+
+enum PLAYERSTATE
+{
+	PLAYER_IDLE = 0,
+	PLAYER_RUN,
+	PLAYER_JUMP,
+	PLAYER_DOUBLEJUMP,
+	PLAYER_ATT,
+	PLAYER_ATT2,
+	PLAYER_DASH
+};
+
+class player : public gameNode
+{
+private:
+	PLAYERSTATE _playerState;
+
+	float playerX, playerY;
+	float jumpSpeed;
+	float playerSpeed;
+	float gravity;
+	bool isJump;
+	int jumpCount;
+	int attCount;
+	int attIndex;
+	RECT rcPlayer;
+	//공격
+	bool isEndAtt1;
+	bool isAtt1;
+	bool isAtt2;
+	bool isAttCool;
+	int Att1CoolDown;
+	int Att2CoolDown;
+	//대쉬
+	int Efect1;
+	int Efect2;
+	int Efect3;
+	int Efect4;
+	int Efect5;
+	int saveX[5];
+	int saveY[5];
+	bool isDash;
+
+	int dashCount;
+	int dashCooldown;
+
+	//충돌
+	POINT ptRight;
+	POINT ptLeft;
+	POINT ptTop;
+	POINT ptBottom;
+	RECT rcRight;
+	RECT rcLeft;
+	RECT rcTop;
+	RECT rcBottom;
+	bool isColl;
+
+	//플레이어 카메라
+	RECT rcPlayerCamera;
+
+	bool isLeft;
+	int animationCount;
+	int animationIndex;
+
+	image* playerRunImg;
+	image* playerIdleImg;
+	image* playerJumpImg;
+	image* playerDashImg;
+	image* playerDashEfectImg1;
+	image* playerAttImg1;
+	image* playerAttImg2;
+
+public:
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+	void PlayerAnimation();
+
+	bool getIsJump() { return isJump; }
+	bool getIsLeft() { return isLeft; }
+	bool getIsDash() { return isDash; }
+	RECT getRcPlayer() { return rcPlayer; }
+	RECT getRcPlayerCamera() { return rcPlayerCamera; }
+	RECT getRcRight() { return rcRight; }
+	RECT getRcLeft() { return rcLeft; }
+	RECT getRcBottom() { return rcBottom; }
+	RECT getRcTop() { return rcTop; }
+	
+	void setPlayerY(float Y) { playerY = Y; }
+	void setPlayerX(float X) { playerX = X; }
+	void setJumpSpeed(float speed) { jumpSpeed = speed; }
+	void setPlayerSpeed(float speed) { playerSpeed = speed; }
+	void setGravity(float gv) { gravity = gv; }
+	void setIsJump(bool jump) { isJump = jump; }
+	void setState(PLAYERSTATE state) { _playerState = state; }
+	void setCameraRight(int right) { rcPlayerCamera.right = right; }
+	void setCameraLeft(int left) { rcPlayerCamera.left = left; }
+	void setJumpCount(int count) { jumpCount = count; }
+	void setIsColl(bool coll) { isColl = coll; }
+};
+
