8c81e14273bf88b2962ea924e60fdc5df6695e26
chldlwns12 <chldlwns12@gmail.com>
2020-05-07 19:30:07 +0900

    2020.05.07 버전 0.36 에너미 클래스에서 에너미 이미지 띄우고 이동 및 공격 모션 확인 픽셀충돌을 이용해 플레이어 이동

---
+++ v0.36/map2.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "gameNode.h"
+class map2 : public gameNode
+{
+private:
+	image* backGroundImg1;
+	image* backGroundImg2;
+	image* backGroundImg3;
+	image* backGroundImg4;
+	image* object;
+	image* front;
+
+	RECT rcBg1;
+	RECT rcBg2;
+	RECT rcBg3;
+	RECT rcBg4;
+
+	//충돌박스
+	RECT rcColl1;
+
+	float tileX, tileY;
+public:
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+};
+
